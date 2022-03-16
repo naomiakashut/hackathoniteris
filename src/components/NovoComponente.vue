66db70909559949ec6f29f46bb6074b1cead95d9
Antonieta Naomi Bazan <bazan.antonieta@gmail.com>
2022-03-16 15:17:40 -0300

    alterações

--- src/components/NovoComponente.vue
+++ src/components/NovoComponente.vue
@@ -1,16 +1,25 @@
 <template>
-  <div id="App">
-    <NovoComponente/>
+  <div class="contador">
+    <h1>Contador: {{ i }}</h1>
+   <button @click="diminuir">-</button><button @click="aumentar">+</button>
   </div>
 </template>
-​
+
 <script>
-import NovoComponente from './components/NovoComponente.vue'
-​
 export default {
-  name: 'App',
-  components: {
-    NovoComponente
+  name: 'Contador-vue',
+  data() {
+    return {
+      i: 0
+    }
+  },
+  methods: {
+    aumentar() {
+      this.i++;
+    },
+    diminuir() {
+      this.i--;
+    }
   }
 }
 </script>
