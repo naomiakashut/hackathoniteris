bf9993eff868cfc7ce7a3c74e8057d9bf59f9451
Antonieta Naomi Bazan <bazan.antonieta@gmail.com>
2022-03-16 13:17:40 -0300

    mudanças

---
+++ src/components/NovoComponente.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="App">
+    <NovoComponente/>
+  </div>
+</template>
+​
+<script>
+import NovoComponente from './components/NovoComponente.vue'
+​
+export default {
+  name: 'App',
+  components: {
+    NovoComponente
+  }
+}
+</script>
